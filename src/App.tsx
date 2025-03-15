87c242c78908ee9542a04c690c26d7f8a3e8aedf
bburcher <bburcher@gmu.edu>
2025-03-15 10:08:31 -0400

    set up new paths for ms2

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Contact from "./components/Contact/Contact";
-import Header from "./components/Header/Header";
-import Home from "./components/Home/Home";
-import Tools from "./components/Tools/Tools";
+import Contact from './components/Contact/Contact';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Tools from './components/Tools/Tools';
 
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
-    <Router basename="/online-budgeting-tool">
+    <Router basename="/online-budgeting-tool-milestone2">
       <Header /> {/* Header component with navbar */}
       <Routes>
         <Route path="/" element={<Home />} /> {/* Home page */}
-        <Route path="tools/*" element={<Tools />} />{" "}
-        <Route path="contact" element={<Contact />} />{" "}
-        {/* About page (example) */}
+        <Route path="tools/*" element={<Tools />} /> <Route path="contact" element={<Contact />} /> {/* About page (example) */}
         {/* Add more routes as needed */}
       </Routes>
     </Router>
